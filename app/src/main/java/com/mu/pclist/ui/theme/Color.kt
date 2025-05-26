39c681df0101a102cd3637ac9d36db6c78986dd5
YurijM <y-myagkov@mail.ru>
2025-05-26 16:31:31 +0300

    Добавлена цветовая схема.

--- app/src/main/java/com/mu/pclist/ui/theme/Color.kt
+++ app/src/main/java/com/mu/pclist/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.mu.pclist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Color0 = Color(0xFFE3F2FD)
+val Color1 = Color(0xFFBBDEFB)
+val Color2 = Color(0xFF90CAF9)
+val Color3 = Color(0xFF64B5F6)
+val Color4 = Color(0xFF42A5F5)
+val Color5 = Color(0xFF2196F3)
+val Color6 = Color(0xFF1E88E5)
+val Color7 = Color(0xFF1976D2)
+val Color8 = Color(0xFF1565C0)
+val Color9 = Color(0xFF0D47A1)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ColorError = Color(0xFFFF0000) 
--- app/src/main/java/com/mu/pclist/ui/theme/Theme.kt
+++ app/src/main/java/com/mu/pclist/ui/theme/Theme.kt
@@ -1,46 +1,43 @@
 package com.mu.pclist.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = Color1,
+    onBackground = Color5,
+    surface = Color0,
+    onSurface = Color9,
+    surfaceVariant = Color2,
+    primary = Color7,
+    primaryContainer = Color1,
+    onPrimary = Color0,
+    onPrimaryContainer = Color7,
+    outline = Color7,
+    onError = ColorError
 )
+/*private val DarkColorScheme = darkColorScheme(
+    background = Color8,
+    surface = Color9,
+    onSurface = Color0,
+    surfaceVariant = Color7,
+    primary = Color3,
+    primaryContainer = Color8,
+    onPrimary = Color9,
+    onPrimaryContainer = Color2,
+    outline = Color3,
+    onError = ColorError
+)*/
 
 @Composable
 fun PCListTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    //darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    //dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+    /*val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -48,7 +45,8 @@ fun PCListTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
+    }*/
+    val colorScheme = LightColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
