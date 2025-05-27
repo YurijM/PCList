ba8895ab2afa1f90dee95a0193651ca4fb141a78
YurijM <y-myagkov@mail.ru>
2025-05-27 10:09:24 +0300

    Добавлены картинки. Обновлён компонент ApplicationBar. Отредактирован SplashScreen.

--- app/src/main/java/com/mu/pclist/presentation/component/ApplicationBar.kt
+++ app/src/main/java/com/mu/pclist/presentation/component/ApplicationBar.kt
@@ -1,9 +1,11 @@
 package com.mu.pclist.presentation.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -12,9 +14,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.mu.pclist.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -33,15 +39,15 @@ fun ApplicationBar() {
                     Text(
                         text = stringResource(R.string.app_name),
                         fontWeight = FontWeight.Bold,
-                        //fontSize = 28.sp,
+                        fontSize = 28.sp,
                         letterSpacing = .15.em
                     )
-                    /*Image(
-                        painter = painterResource(id = R.drawable.fish),
+                    Image(
+                        painter = painterResource(id = R.drawable.pc),
                         contentDescription = null,
                         modifier = Modifier.padding(vertical = 4.dp),
                         contentScale = ContentScale.FillHeight,
-                    )*/
+                    )
                 }
             },
             colors = TopAppBarDefaults.topAppBarColors(
--- app/src/main/java/com/mu/pclist/presentation/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/mu/pclist/presentation/screen/splash/SplashScreen.kt
@@ -1,24 +1,165 @@
 package com.mu.pclist.presentation.screen.splash
 
-import androidx.compose.foundation.clickable
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mu.pclist.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
     toMain: () -> Unit
 ) {
-    Text(
-        text = "Splash Screen",
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 80.dp)
-            .clickable { toMain() }
+    val infiniteTransition = rememberInfiniteTransition()
+    val angle = infiniteTransition.animateFloat(
+        initialValue = 0F,
+        targetValue = 360F,
+        animationSpec = infiniteRepeatable(
+            animation = tween(2000, easing = LinearEasing)
+        )
     )
+    val scale = remember {
+        Animatable(0f)
+    }
+    val scalePC = remember {
+        Animatable(0f)
+    }
+    val scaleText = remember {
+        Animatable(0f)
+    }
+
+    val developmentText = stringResource(R.string.design) + "\n"
+    val companyText = stringResource(R.string.company)
+    val text = buildAnnotatedString {
+        append("$developmentText ")
+        withStyle(
+            style = SpanStyle(
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        ) {
+            pushStringAnnotation(tag = companyText, annotation = companyText)
+            append(companyText)
+        }
+    }
+
+    LaunchedEffect(key1 = true) {
+        scalePC.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 3000,
+                easing = {
+                    OvershootInterpolator(5f).getInterpolation(it)
+                }
+            )
+        )
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 2000,
+                easing = {
+                    OvershootInterpolator(10f).getInterpolation(it)
+                }
+            )
+        )
+        scaleText.animateTo(
+            targetValue = .85f,
+            animationSpec = tween(
+                durationMillis = 2000,
+                easing = {
+                    OvershootInterpolator(5f).getInterpolation(it)
+                }
+            )
+        )
+        delay(1000L)
+        toMain()
+    }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 28.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.pc1),
+                contentDescription = "Title",
+                modifier = Modifier
+                    .weight(1f)
+                    /*.graphicsLayer {
+                        rotationZ = angle.value
+                    }*/
+                    .scale(scalePC.value)
+            )
+            Text(
+                text = stringResource(R.string.app_name),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.displayMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 40.dp)
+            )
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1.5f),
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.author),
+                    contentDescription = "Logo",
+                    modifier = Modifier.scale(scale.value)
+                        .rotate(angle.value),
+                )
+                AssistChip(
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .scale(scaleText.value),
+                    onClick = {},
+                    label = {
+                        Text(text = text)
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "settings"
+                        )
+                    }
+                )
+            }
+        }
+    }
 }
