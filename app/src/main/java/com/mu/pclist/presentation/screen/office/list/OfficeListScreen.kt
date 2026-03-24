cac6644358ec0049c6c52a83f8b924db5b417e6b
YurijM <y-myagkov@mail.ru>
2026-03-24 16:45:27 +0300

    Тестирование создания текстового файла.

--- app/src/main/java/com/mu/pclist/presentation/screen/office/list/OfficeListScreen.kt
+++ app/src/main/java/com/mu/pclist/presentation/screen/office/list/OfficeListScreen.kt
@@ -1,24 +1,31 @@
 package com.mu.pclist.presentation.screen.office.list
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -35,6 +42,7 @@ fun OfficeListScreen(
     toOffice: (OfficeDestination) -> Unit
 ) {
     val offices by viewModel.offices.collectAsState(initial = null)
+    val context = LocalContext.current
 
     if (offices == null) {
         Box(
@@ -54,8 +62,36 @@ fun OfficeListScreen(
                 ),
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Title(stringResource(R.string.offices) +
-                        if (offices!!.isNotEmpty()) " (${offices!!.size})" else "")
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxWidth(.8f)
+                    ) {
+                        Title(
+                            stringResource(R.string.offices) +
+                                    if (offices!!.isNotEmpty()) " (${offices!!.size})" else ""
+                        )
+                    }
+
+                    IconButton(
+                        onClick = { viewModel.createFile(context) },
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .border(
+                                    1.dp,
+                                    MaterialTheme.colorScheme.primary,
+                                    shape = RoundedCornerShape(50)
+                                )
+                                .padding(5.dp)
+                        )
+                    }
+                }
                 HorizontalDivider(
                     thickness = 1.dp,
                     color = MaterialTheme.colorScheme.primary,
--- app/src/main/java/com/mu/pclist/presentation/screen/office/list/OfficeListViewModel.kt
+++ app/src/main/java/com/mu/pclist/presentation/screen/office/list/OfficeListViewModel.kt
@@ -1,11 +1,14 @@
 package com.mu.pclist.presentation.screen.office.list
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mu.pclist.data.entity.OfficeEntity
 import com.mu.pclist.domain.repository.OfficeRepository
+import com.mu.pclist.presentation.util.toLog
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,6 +16,33 @@ class OfficeListViewModel @Inject constructor(
     private val officeRepository: OfficeRepository
 ) : ViewModel() {
     var offices = officeRepository.officeList()
+
+    fun createFile(context: Context) {
+        val path = context.filesDir
+        toLog("path: $path")
+
+        //External Storage
+        /*val path = context.getExternalFilesDir(null)
+        <uses-permission android-name="android.permission.WRITE_EXTERNAL_STORAGE" />*/
+
+        val newDir = File(path, "Test")
+        toLog("newDir: $newDir")
+        //toLog("mkdirs: ${newDir.mkdirs()}")
+
+        var file = File(newDir, "Qwerty.txt")
+        toLog("file: ${file.path}")
+
+        //context.openFileOutput("Qwerty.txt", Context.MODE_PRIVATE).use {
+        file.outputStream().use {
+            it.write("Test qwerty 2".toByteArray())
+            toLog("write")
+        }
+
+        file = File(newDir, "Qwerty.txt")
+        //val file = File(path, "Qwerty.txt")
+        val content = file.readText()
+        toLog("read: $content")
+    }
 
     fun onEvent(event: OfficeListEvent) {
         when (event) {
